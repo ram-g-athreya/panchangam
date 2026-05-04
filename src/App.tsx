a0a38b3c34ae15ac6b74753f067f607e4dedbc00
TheButterThief <3251705+ram-g-athreya@users.noreply.github.com>
2026-05-03 22:46:46 -0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-
-function App() {
+export function App() {
   return (
     <div>
       <h1>Panchangam</h1>
     </div>
-  )
+  );
 }
-
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Root element not found')
-
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
